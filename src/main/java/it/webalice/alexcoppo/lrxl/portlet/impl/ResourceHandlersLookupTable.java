5f6ab96399ef52fd40ebd4789796535be508ea60
alexcoppo <alexcoppo@alice.it>
2013-03-19 15:04:51 +0100

    Initial add

---
+++ src/main/java/it/webalice/alexcoppo/lrxl/portlet/impl/ResourceHandlersLookupTable.java
@@ -0,0 +1,75 @@
+/*
+    Copyright (c) 2013 Alessandro Coppo
+    All rights reserved.
+
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions
+    are met:
+    1. Redistributions of source code must retain the above copyright
+       notice, this list of conditions and the following disclaimer.
+    2. Redistributions in binary form must reproduce the above copyright
+       notice, this list of conditions and the following disclaimer in the
+       documentation and/or other materials provided with the distribution.
+    3. The name of the author may not be used to endorse or promote products
+       derived from this software without specific prior written permission.
+
+    THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+    IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+    OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+    IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+    INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+    NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+    DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+    THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+    (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+    THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+package it.webalice.alexcoppo.lrxl.portlet.impl;
+
+import it.webalice.alexcoppo.lrxl.portlet.ResourceMapping;
+import java.lang.reflect.Method;
+import javax.portlet.ActionRequest;
+import javax.portlet.BaseURL;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+import javax.portlet.ResourceRequest;
+import javax.portlet.ResourceResponse;
+import net.sf.jautl.utility.reflection.MethodUtils;
+
+/**
+ *
+ */
+public class ResourceHandlersLookupTable extends HandlersLookupTable {
+    public ResourceHandlersLookupTable(Object obj) {
+        super(obj.getClass());
+    }
+    
+    @Override
+    public String match(Method method) {
+        if (!checkMethodSignature(method, ResourceRequest.class, ResourceResponse.class))
+            return null;
+
+        if (!MethodUtils.hasAnnotation(method, ResourceMapping.class))
+            return null;
+
+        ResourceMapping map = (ResourceMapping)method.getAnnotation(ResourceMapping.class);
+        
+        return map.command();
+    }
+
+    public void inject(RenderRequest request, RenderResponse response) {
+        for (String name : handlers.keySet()) {
+            Method method = handlers.get(name);
+            ResourceMapping rm = (ResourceMapping)MethodUtils.getAnnotation(method, ResourceMapping.class);
+            inject(rm, request, response);
+        }
+    }
+
+    private void inject(ResourceMapping rm, RenderRequest request, RenderResponse response) {
+        BaseURL url = response.createResourceURL();
+        
+        url.setParameter(ActionRequest.ACTION_NAME, rm.command());
+        
+        request.setAttribute(ResourceMappingUtils.getDecoratedName(rm), url);
+    }
+}
